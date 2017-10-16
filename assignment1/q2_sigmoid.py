041d8c218224f91b76d645cd1785247a52208156
Ted Yun <ted.yun@oracle.com>
2017-10-16 13:39:15 -0400

    solving q2_sigmoid

--- assignment1/q2_sigmoid.py
+++ assignment1/q2_sigmoid.py
@@ -15,7 +15,9 @@ def sigmoid(x):
     """
 
     ### YOUR CODE HERE
-    raise NotImplementedError
+    
+    s = 1 / (1 + np.exp(-x))
+    
     ### END YOUR CODE
 
     return s
@@ -35,7 +37,7 @@ def sigmoid_grad(s):
     """
 
     ### YOUR CODE HERE
-    raise NotImplementedError
+    ds = s * (1 - s)
     ### END YOUR CODE
 
     return ds
@@ -72,7 +74,9 @@ def test_sigmoid():
     """
     print "Running your tests..."
     ### YOUR CODE HERE
-    raise NotImplementedError
+    
+    # TODO
+
     ### END YOUR CODE
 
 
